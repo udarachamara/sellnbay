34396c2c676db20da6d9a3cd02fc08f220483bc8
Safnaj <safnaj99@gmail.com>
2019-05-19 14:59:19 +0530

    OrderConfig Developed

--- src/main/java/com/paf/configuration/OrderConfig.java
+++ src/main/java/com/paf/configuration/OrderConfig.java
@@ -1,5 +1,24 @@
 package com.paf.configuration;
 
-public class OrderConfig {
+import javax.sql.DataSource;
 
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories(entityManagerFactoryRef = "orderEntityManagerFactory",
+transactionManagerRef = "orderTransactionManager",basePackages = {"com.paf.repository.order"})
+
+public class OrderConfig {
+	
+	 @Bean(name = "orderDataSource")
+	  @ConfigurationProperties(prefix = "udara.datasource")
+	  public DataSource dataSource() {
+	    return DataSourceBuilder.create().build();
+	  }
 }
